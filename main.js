03ca2064c1bd9089e1c0c0c7e146fe2377f4da12
Linnea Åkesson <linnea.akesson@hyperisland.se>
2023-11-10 14:46:26 +0100

    Update

--- main.js
+++ main.js
@@ -5,12 +5,15 @@ let lastGuess = null; // To keep track of the last guess
 
 // Generate a random number between 1 and 100
 let number = Math.floor(Math.random() * 100) + 1;
+console.log(number);
 
 // Sound effects
 let correctSound = new Audio('Sounds/twinklesparkle.mp3');
 let wrongSound = new Audio('Sounds/blaster.mp3');
 
 let userInput = document.getElementById('userinput');
+
+userInput.focus();
 
 userInput.addEventListener('keypress', function(event) {
   if (event.key === 'Enter') {
@@ -33,16 +36,21 @@ btn.addEventListener('click', function() {
       setTimeout(() => {
         document.getElementById('container').classList.remove('shake'); // Remove shake class after 0.5 seconds
       }, 500);
-    } else if (input < number) {
-      output.innerHTML = "You guessed too low. Try again!";
-      wrongSound.play(); // Play wrong guess sound
-    } else {
-      output.innerHTML = "You guessed too high! Try again!";
-      wrongSound.play(); // Play wrong guess sound
-      document.getElementById('container').classList.add('shake-small'); // Add small shake class
-      setTimeout(() => {
-        document.getElementById('container').classList.remove('shake-small'); // Remove small shake class after 0.1 seconds
-      }, 100);
-    }
+      } else if (input < number) {
+        output.innerHTML = "You guessed too low. Try again!";
+        wrongSound.play(); // Play wrong guess sound
+        
+      } else {
+        output.innerHTML = "You guessed too high! Try again!";
+        wrongSound.play(); // Play wrong guess sound
+        document.getElementById('container').classList.add('shake-small'); // Add small shake class
+        setTimeout(() => {
+          document.getElementById('container').classList.remove('shake-small'); // Remove small shake class after 0.1 seconds
+        }, 100);
+      }
   }
+  userInput.value = "";
 });
+
+
+
